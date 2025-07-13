a90024aeb221ef0e1b41ed204637297c866ca9d2
CatGamer7 <arthur.zayda@gmail.com>
2025-07-13 22:43:27 +0700

    add: respond() unit test

--- src/web/request/tests.rs
+++ src/web/request/tests.rs
@@ -1,6 +1,6 @@
-use std::{io::Cursor, iter::zip};
+use std::{io::{Cursor, Read, Seek, SeekFrom}, iter::zip};
 
-use crate::web::{request::{RequestBackend, StatusRequest}, response::{ResponseCode, Versions}};
+use crate::web::{request::{RequestBackend, StatusRequest}, response::{Header, Response, ResponseCode, Versions}};
 
 #[test]
 /// Test [StatusRequest] build method in normal operation.
@@ -78,4 +78,49 @@ fn build_request_fail() {
     );
     let req = RequestBackend::build(stream).unwrap_err();
     assert!(req == ResponseCode::get_400(), "must fail on invalid format (missing cr, nl after headers)");
+}
+
+#[test]
+/// Test [RequestBackend] respond method.
+fn respond() {
+    let stream: MockStream = Cursor::new(
+        Vec::from(
+            "GET /test HTTP/1.1\r\nHost: www.example.com\r\nAccept-Language: en\r\n\r\n{\"meow\": 1}".as_bytes()
+        )
+    );
+    let mut req: RequestBackend<MockStream> = RequestBackend::build(stream).unwrap();
+
+    // Test that the stream was consumed correctly.
+    let mut buf = Vec::new();
+    let mut stream_copy = req.get_response_stream().clone();
+    let read_eof = stream_copy.read(&mut buf).unwrap();
+    assert!(read_eof == 0);
+
+    let resp = Response::new(
+        Versions::Http1_1,
+        ResponseCode::get_200(),
+        vec![
+            Header::build(
+                "Content-Type: text/plain".to_string()
+            ).unwrap(),
+            Header::build(
+                "Content-Length: 5".to_string()
+            ).unwrap()
+        ],
+        Vec::from(
+            "Hello"
+        )
+    );
+    req.respond(&resp);
+
+    // Test response that was written.
+    let mut buf = Vec::new();
+    let mut stream_copy = req.get_response_stream().clone();
+    stream_copy.seek(SeekFrom::Start(0)).unwrap();
+    stream_copy.read_to_end(&mut buf).unwrap();
+    let actual_resp = Vec::from(
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 5\r\n\r\nHello".as_bytes()
+    );
+
+    assert!(buf == actual_resp);
 }
